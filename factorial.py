f94441a1c4a71c92c7c717bb9117a073c87d9ca1
Krostofer <christopher.di-bert@stu.ocsb.ca>
2022-11-01 21:02:53 +0000

    This program calculates the factorial value of an integer.

---
+++ factorial.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+# Created By: Chris Di Bert
+# Date: Oct. 26, 2022
+# This program outputs the sum of all numbers preceding the user's
+# number using a while loop.
+
+
+def main():
+
+    # Initializing variables
+    loop_counter = 0
+    factorial_ans = 1
+
+    # Gets the user's number as a string
+    str_num = input("Enter a positive integer: ")
+
+    # Tries casting the user's number to an integer
+    try:
+        int_num = int(str_num)
+
+    # Tells the user to restart if they did not enter a number
+    except ValueError:
+        input("You must enter an integer. Please try again.")
+
+    # Tells the user to restart if their number is negative
+    if int_num < 0:
+        input("You must enter a positive number. Please restart.")
+
+    # Loops through body code until the counter is greater than
+    # the user's number.
+    while True:
+        loop_counter += 1
+        factorial_ans *= loop_counter
+        print(f"Looped through {loop_counter} times.")
+        if loop_counter >= int_num:
+            break
+
+    # Prints the factorial value of the user's number.
+    print(f"{int_num}! = {factorial_ans}")
+
+
+if __name__ == "__main__":
+    main()
